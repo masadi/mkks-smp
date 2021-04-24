5964494bfe18d063069c43715e635f2aed79aee5
masadi <masadi.com@gmail.com>
2021-04-25 03:48:02 +0700

    Update footer.blade.php

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,10 +1,6 @@
 <!-- Main Footer -->
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-        Versi 2.1
-    </div>
+<footer class="main-footer text-center">
     <!-- Default to the left -->
-    <strong>Hak Cipta &copy; {{ now()->year }} Direktorat SMK</strong>. All rights reserved.
+    <strong>Hak Cipta &copy; {{ now()->year }} Pusmenjar Kemdikbud</strong>. All rights reserved.
 
 </footer>
